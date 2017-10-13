55bde18ef8a1b9692191507c233f60db9c39ea36
vinilshakerz <vadimbratanov@gmail.com>
2017-10-13 22:56:47 +0300

    fix name

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,14 +5,14 @@ import newGame from '..';
 const description = 'Balance the given number.';
 const maxNum = 10000;
 const additionalDigit = 1;
-const numLenght = num => String(num).length;
-const exponentOfTen = exp => 10 ** exp;
-const order = (digit, exp) => digit * exponentOfTen(exp);
+const getNumLenght = num => String(num).length;
+const getExponentOfTen = exp => 10 ** exp;
+const order = (digit, exp) => digit * getExponentOfTen(exp);
 /* Cложения цифр числа */
 const foldNum = (num) => {
   /* numDigit - порядковый номер символа , sum - сумма цифр */
   const iter = (numDigit, sum) => {
-    if (numDigit === numLenght(num)) {
+    if (numDigit === getNumLenght(num)) {
       return sum;
     }
     const digit = Number(String(num)[numDigit]);
@@ -21,9 +21,9 @@ const foldNum = (num) => {
   return iter(0, 0);
 };
 /* Строит число с длиной символов numLength из цифр digit (по умолчанию цифра 1) */
-const constructNum = (digit, numberLenght) => {
+const constructNum = (digit, numLenght) => {
   const iter = (acc, exp) => {
-    if (exp === numberLenght) {
+    if (exp === numLenght) {
       return acc;
     }
     return iter(acc + order(digit, exp), exp + 1);
@@ -32,15 +32,15 @@ const constructNum = (digit, numberLenght) => {
 };
 /* mainDigit вычисляет цифру из которой строится число без учета
 дополнительных единиц. */
-const mainDigit = num => Math.floor(foldNum(num) / numLenght(num));
+const getMainDigit = num => Math.floor(foldNum(num) / getNumLenght(num));
 /* additionalDigitLenght количество оставшихся единиц  */
-const additionalDigitLenght = num => foldNum(num) % numLenght(num);
+const getAdditionalDigitLenght = num => foldNum(num) % getNumLenght(num);
 const makeBalance = (num) => {
-  if (foldNum(num) < numLenght(num)) {
+  if (foldNum(num) < getNumLenght(num)) {
     return constructNum(additionalDigit, foldNum(num));
   }
-  const mainNum = constructNum(mainDigit(num), numLenght(num));
-  const additionalNum = constructNum(additionalDigit, additionalDigitLenght(num));
+  const mainNum = constructNum(getMainDigit(num), getNumLenght(num));
+  const additionalNum = constructNum(additionalDigit, getAdditionalDigitLenght(num));
   return mainNum + additionalNum;
 };
 const makeGameParameters = () => {
