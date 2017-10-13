0877c15b303ecc02488dfe725cef6bb0d01ef43c
vinilshakerz <vadimbratanov@gmail.com>
2017-10-14 01:49:59 +0300

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import { cons } from 'hexlet-pairs';
+import getRandomNum from '../utils';
+import newGame from '..';
+
+const description = 'Answer "yes" if number prime otherwise answer "no".';
+const halfNum = num => Math.floor(num / 2) + 1;
+const isDivider = (num, div) => num % div === 0;
+const isPrime = (num) => {
+  const iter = (div) => {
+    if (isDivider(num, div)) {
+      return false;
+    }
+    if (div > halfNum(num)) {
+      return true;
+    }
+    return iter(div + 2);
+  };
+  return iter(2);
+};
+const makeGameParameters = () => {
+  const question = getRandomNum();
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, String(correctAnswer));
+};
+const prime = () => newGame(makeGameParameters, description);
+export default prime;
